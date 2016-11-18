fae5dbf16740e35e29dcc8ce8805543f888db6ef
Jonathan Santos <santojon5@gmail.com>
2016-11-18 16:55:07 +0000

    fix home with session user

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,5 +1,12 @@
 with (Sgfd.Base) {
     var HomeControler = {
-        
+        /**
+         * Assert all is clear
+         */
+        index: () => {
+            unselectItem('my-things-click')
+            unselectItem('login-click')
+            unselectItem('tournament-click')
+        }
     }
 }
