bf50b8744bedc4e4f9ccb5dc2ff20bba5a3534b1
crvzybird <rtulyo@hotmail.com>
2016-12-05 13:31:30 -0300

    texte de texto corrido

--- data/content/homecontent.js
+++ data/content/homecontent.js
@@ -15,6 +15,10 @@ var homeContent = {
             img: 'assets/images/hearthstone_event.png',
             paragraphs: [
                 'Rule 1', 
+                'ndsfnçaeifsdnçssdfndsçfsdfnsdnçsdfksnfnsdsn~fskdfnsdgn\
+                asfsddsd.Asdfsdkfsldkfçsglfmg sdff werwefs serweewee.\
+                frwewe.AAAAdasmdsewFDLGSEK SDFSFE AE . aDSFSDKE EWFRWEEW S\
+                DDEFDSsdfsd sdf e wfs eggrgrwe...',
                 'fwrer',
                 'Rule 2',
                 'Rule 3'
