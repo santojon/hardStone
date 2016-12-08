8682a8fb7441b35ac4d5e89d06b3652b06346d83
crvzybird <rtulyo@hotmail.com>
2016-12-08 11:11:16 -0300

    Mudei arquivos de imagens, e suas respectivas chamadas no código.Também adicionei texto nas sessões.

--- data/content/homecontent.js
+++ data/content/homecontent.js
@@ -4,39 +4,59 @@ var homeContent = {
     sections: [
         {
             title: 'About',
-            img: 'assets/images/classic-cardback.png',
+            img: 'assets/images/Anunciador_2.png',
             paragraphs: [
                 'About 1',
-                'About 2'
+                'Psst...Eu tenho um ótimo negócio para você... - Negociante Escuso'
             ]
         },
         {
             title: 'Rules',
-            img: 'assets/images/hearthstone_event.png',
+            img: 'assets/images/rules_image.png',
             paragraphs: [
-                'Rule 1', 
-                'ndsfnçaeifsdnçssdfndsçfsdfnsdnçsdfksnfnsdsn~fskdfnsdgn\
-                asfsddsd.Asdfsdkfsldkfçsglfmg sdff werwefs serweewee.\
-                frwewe.AAAAdasmdsewFDLGSEK SDFSFE AE . aDSFSDKE EWFRWEEW S\
-                DDEFDSsdfsd sdf e wfs eggrgrwe...',
-                'fwrer',
-                'Rule 2',
-                'Rule 3'
+                'Rule 1 :', 
+                ' - Os jogadores só poderão usar decks que sejam compostos\
+                 somente pelas edições Básicas e Clássicas...',
+                'Rule 2 :',
+                ' - Todos os jogadores deverão criar quatro decks, de classes\
+                 distintas.O adversário então irá escolher um dentre os decks\
+                 para ser banido(para não fazer parte da partida)..',
+                'Rule 3 :',
+                ' - O modo de jogo seguirá o formato "Conquest" criado pela\
+                Blizzard em que a vitória da partida será concedida áquele\
+                que conseguir vencer três rounds na partida.Entre rounds, o\
+                 deck vencedor não poderá ser usado na partida, já o que \
+                 perdeu pode ser selecionado novamente...',
+                 'Segue as regras!! - Pacificador Aldor'
             ]
         },
         {
             title: 'Classes',
-            img: 'assets/images/icons.png',
+            img: 'assets/images/classes_3.png',
             paragraphs: [
-                'Paragraph 1',
-                'Paragraph 2'
+                'Serão permitidos todas as classes existentes dentro do jogo,\
+                 mas não serão permitidos skins...',
+                'Paladino - Lutarei com honra!',
+                'Druida - Eu preciso proteger a Natureza!',
+                'Caçador - Que a caçada comece!',
+                'Sacerdote - A Luz trará a vitória!',
+                'Ladino - É melhor abrir o olho!',
+                'Bruxo - Sua alma será minha!',
+                'Mago - Foi você que pediu!',
+                'Xamã - Pelo Martelo da Perdição!',
+                'Guerreiro - Vitória ou morte!',
+                
             ]
         },
         {
             title: 'Prizes',
-            img: 'assets/images/classic-cardback.png',
+            img: 'assets/images/prize_image.png',
             paragraphs: [
-                'Prize?'
+                'Ao fim do campeonato, os jogadores que alcançarem o primeiro\
+                  segundo e terceiro lugar receberão uma premiação em dinheiro\
+                  correspondente á 50%, 25% e 15% do total arrecadado pelas\
+                  inscrições, respectivamente...',
+                  'Quem quer presentes? - Blingtron 3000'
             ]
         }
     ]
